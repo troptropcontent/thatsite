a57bdd0e2d87cec77e12c48c8415bbf0f98c825d
Tom Ecrepont <tomecrepont@gmail.com>
2022-01-31 08:35:45 +0100

    set the page controller to now return a 500 if the record is not found and use the page layout instead of the application layout

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class PageController < ApplicationController
-  before_action :find_page
+  before_action :load_page
 
   def show
-    @data = show_service.new(@page).call!
+    @page
+    render @page.name, layout: 'page'
   end
 
   private
@@ -13,11 +14,7 @@ class PageController < ApplicationController
     params.permit(:site_id, :name)
   end
 
-  def find_page
-    @page = Page.find_by(page_params)
-  end
-
-  def show_service
-    @page.instance_of?(Custom) ? CustomPageGenerator : DefaultPageGenerator
+  def load_page
+    @page = Page.find_by!(page_params)
   end
 end
