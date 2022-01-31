21003cdd4cc23b82ada84c911f363089c1f43e98
Tom Ecrepont <tomecrepont@gmail.com>
2022-01-31 08:32:47 +0100

    re;ove the validation on single quote for fonts

--- app/models/font_pair.rb
+++ app/models/font_pair.rb
@@ -1,4 +1,2 @@
 class FontPair < ApplicationRecord
-    validates :primary, presence: true, format: { with: /'*'/, message: "font name should be single quoted"}
-    validates :secondary, presence: true, format: { with: /'*'/, message: "font name should be single quoted"}
 end
