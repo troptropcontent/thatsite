6df2a0411f6ff5c1187f8a629a2445981bc4aef1
Tom Ecrepont <tomecrepont@gmail.com>
2022-01-31 08:34:35 +0100

    create a seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+color_palette = FactoryBot.create(:color_palette)
+font_pair = FactoryBot.create(:font_pair)
+business = FactoryBot.create(:business)
+site = FactoryBot.create(:site, color_palette: color_palette, font_pair: font_pair, business: business)
+contact = FactoryBot.create(:contact, site: site)
+home = FactoryBot.create(:home, site: site)
+about = FactoryBot.create(:about, site: site)
+
+color_palette = FactoryBot.create(
+    :color_palette,
+    first: "e63946",
+    second: "f1faee",
+    third: "a8dadc",
+    fourth: "457b9d",
+    fifth: "1d3557"
+)
+font_pair = FactoryBot.create(
+    :font_pair,
+    link_tag_href: "https://fonts.googleapis.com/css2?family=Libre+Baskerville&family=Roboto:wght@300&display=swap",
+    primary: "Libre Baskerville",
+    primary_backup: "serif",
+    secondary: "Roboto",
+    secondary_back_up: "sans-serif",
+)
+business = FactoryBot.create(:business, name: "my other business")
+site = FactoryBot.create(:site, color_palette: color_palette, font_pair: font_pair, business: business)
+contact = FactoryBot.create(:contact, site: site)
+home = FactoryBot.create(:home, site: site)
+about = FactoryBot.create(:about, site: site)
