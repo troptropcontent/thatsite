67e295e923b3b522d3bd32b5ff5cc3be3728214f
Tom Ecrepont <tomecrepont@gmail.com>
2022-02-10 07:16:19 +0100

    Create seed (#45)
    * update the seed with the new color palette
    
    * does not allow two sections to have the same name for one page
    
    * change the color palette of the first site
    
    * schema update
    
    * change the name of the top links
    
    * update the seed to not create site if it already exist aand just update sections

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_27_210629) do
+ActiveRecord::Schema.define(version: 2022_02_09_211532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 2022_01_27_210629) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["page_id", "name"], name: "index_sections_on_page_id_and_name", unique: true
     t.index ["page_id"], name: "index_sections_on_page_id"
   end
 
