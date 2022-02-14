e546a1b3e72237791d6f00a2a5638673f4424493
Tom Ecrepont <tomecrepont@gmail.com>
2022-02-14 21:09:13 +0100

    get rid of the uuid id on site (#53)

--- db/migrate/20220123084451_create_sites.rb
+++ db/migrate/20220123084451_create_sites.rb
@@ -1,7 +1,7 @@
 class CreateSites < ActiveRecord::Migration[7.0]
-  enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+  
   def change
-    create_table :sites, id: :uuid, default: 'gen_random_uuid()' do |t|
+    create_table :sites do |t|
       t.references :color_palette, null: false, foreign_key: true
       t.references :font_pair, null: false, foreign_key: true
       t.references :business, null: false, foreign_key: true
--- db/migrate/20220123090932_create_pages.rb
+++ db/migrate/20220123090932_create_pages.rb
@@ -2,7 +2,7 @@ class CreatePages < ActiveRecord::Migration[7.0]
   def change
     create_table :pages do |t|
       t.string :type
-      t.references :site, type: :uuid
+      t.references :site
       t.timestamps
     end
   end
