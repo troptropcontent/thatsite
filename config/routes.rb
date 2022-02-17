5c5a3ad7da643eb0e889fd20014f9fc6c8f66120
Tom Ecrepont <tomecrepont@gmail.com>
2022-02-17 08:13:26 +0100

    Set up route with subdomain (#54)
    * add a unique name to the sites
    
    * update the seed accordingly
    
    * update the route to allow subdomains
    
    * allow lvh domains to be used in development
    
    * update the page controller to now load the page based on the subdomain and name in path
    
    * update header links to simply returns /contact etc
    
    * add specs

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'page#show'
-  # this is for public site of thatsite, with now site param it will return the first page of this name
-  get '/:name', to: 'page#show', as: :page_show
 
-  resources :sites, only: [:show] do
-    get '/:name', to: 'page#show', as: :page_show
-  end
+  get '/:name', to: 'page#show', constraints: { subdomain: /^[a-zA-Z0-9]*/ }
 end
+
