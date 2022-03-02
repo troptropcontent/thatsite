6d868c2c122bc3f6834f485b3139260516177d81
Tom Ecrepont <tomecrepont@gmail.com>
2022-03-02 09:05:53 +0100

    create a content loader service

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -4,6 +4,7 @@ class PageController < ApplicationController
   before_action :load_page_and_site
 
   def show
+    # @content = load_content
     render @page.name, layout: 'page'
   end
 
@@ -17,5 +18,9 @@ class PageController < ApplicationController
     site_name = ['', 'www'].include?(request.subdomain) ? 'thatsite' : request.subdomain
     @site = Site.find_by!(name: site_name)
     @page = @site.pages.find_by!(page_params)
+  end
+
+  def load_content
+    content_loader.new(@page).call
   end
 end
