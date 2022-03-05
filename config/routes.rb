0db3a7154d07e0af204b6ece2062495342cbdfab
Tom Ecrepont <tomecrepont@gmail.com>
2022-03-05 18:45:32 +0100

    create the route and the controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@
 
 Rails.application.routes.draw do
   devise_for :users
+  constraints subdomain: "member" do
+    get "/" => "dashboard#show"
+  end
   root 'page#show'
+
 
   get '/:name', to: 'page#show', constraints: { subdomain: /^[a-zA-Z0-9]*/ }
 end
