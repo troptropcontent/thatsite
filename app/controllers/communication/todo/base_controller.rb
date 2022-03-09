391b74b9fdd1ec45530fe39ec67541970525b410
Tom Ecrepont <tomecrepont@gmail.com>
2022-03-09 09:39:53 +0100

    create an error partial has we might need it later and update the basic_info controller to either return to the same page with error or to the site edit page is the name is OK

--- app/controllers/communication/todo/base_controller.rb
+++ app/controllers/communication/todo/base_controller.rb
@@ -3,10 +3,15 @@ module Communication::Todo
         
         #PUT /todo/basic_info
         def basic_info
-            site = current_user.sites.create!(site_params)
-            new_to_do = BasicInfo.new(site)
+            @site = Site.new(site_params)
             respond_to do |format|
-             format.turbo_stream { render turbo_stream: turbo_stream.replace(new_to_do, partial: 'communication/todo/basic_info', locals: { todo: new_to_do}) }
+                if @site.save
+                    link_site_to_user
+                    format.turbo_stream { redirect_to edit_site_path(@site.name)  }
+                else
+                    new_to_do = BasicInfo.new(@site)
+                    format.turbo_stream { render turbo_stream: turbo_stream.replace(new_to_do, partial: 'communication/todo/basic_info', locals: { todo: new_to_do, errors: [I18n.t("todos.basic_infos.errors.invalid_name")]}) }
+                end
             end
         end
 
@@ -14,6 +19,10 @@ module Communication::Todo
 
         def site_params
             params[:site].permit(:name)
+        end
+
+        def link_site_to_user
+            current_user.sites << @site
         end
     end
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,6 +1,10 @@
 class Site < ApplicationRecord
   belongs_to :color_palette, optional: true
   belongs_to :font_pair, optional: true
+  has_many :sites_user
+  has_many :users, through: :sites_user
   has_one :business
   has_many :pages
+
+  validates :name, uniqueness: true
 end
