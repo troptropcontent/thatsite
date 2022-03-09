671bedb23187348fb6eec0a2b8b449806f2f7f3a
Tom Ecrepont <tomecrepont@gmail.com>
2022-03-09 01:03:40 +0100

    create the site co,troller

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -2,7 +2,29 @@
 
 class SitesController < ApplicationController
     before_action :authenticate_user!
+    before_action :load_site
 
-    # member.thatsite.io/sites/new
-    def new; end
+    # GET member.thatsite.io/sites/new
+    def new
+        @to_dos = Communication::Todo::Base.list(@site)
+    end
+
+    # GET member.thatsite.io/sites/:name/edit
+    def edit
+        @to_dos = Communication::Todo::Base.list(@site)
+    end
+
+    private
+
+    def load_site         
+       if site_params.present?
+            @site ||= current_user.sites.find_by!(site_params)
+       else
+             @site = current_user.sites.new() 
+       end 
+    end
+
+    def site_params
+        params.permit(:name)
+    end
 end
