70451f22bfadd8314a6284228d42b43d58bda89b
Tom Ecrepont <tomecrepont@gmail.com>
2022-03-09 09:40:23 +0100

    remove load site as we do not need it for now

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -2,27 +2,20 @@
 
 class SitesController < ApplicationController
     before_action :authenticate_user!
-    before_action :load_site
 
     # GET member.thatsite.io/sites/new
     def new
+        @site = current_user.sites.new() 
         @to_dos = Communication::Todo::Base.list(@site)
     end
 
     # GET member.thatsite.io/sites/:name/edit
     def edit
+        @site = current_user.sites.find_by!(site_params)
         @to_dos = Communication::Todo::Base.list(@site)
     end
 
     private
-
-    def load_site         
-       if site_params.present?
-            @site ||= current_user.sites.find_by!(site_params)
-       else
-             @site = current_user.sites.new() 
-       end 
-    end
 
     def site_params
         params.permit(:name)
