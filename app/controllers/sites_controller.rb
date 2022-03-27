70e41d462be619e0b7d7bdcdaf2e53c4b88bc24c
Tom Ecrepont <tomecrepont@gmail.com>
2022-03-27 04:21:18 +0200

    assign gloabal variables to controller actions

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -8,6 +8,7 @@ class SitesController < ApplicationController
         @site = current_user.sites.new()
         @other_sites = current_user.sites.pluck(:name)
         @available_color_palettes = ColorPalette.all
+        @available_font_pairs = FontPair.all
         @to_dos = Communication::Todo::Base.list(@site)
         render "form"
     end
@@ -16,24 +17,31 @@ class SitesController < ApplicationController
     def edit
         @site = current_user.sites.find_by!(name: params[:name])
         @other_sites = current_user.sites.where.not(id: @site.id).pluck(:name)
-        @available_color_palettes = ColorPalette.where.not(id: @site.color_palette_id)
+        @available_color_palettes = ColorPalette.all
+        @available_font_pairs = FontPair.all
         @to_dos = Communication::Todo::Base.list(@site)
         render "form"
     end
 
     # PUT member.thatsite.io/sites/:name
     def update
-
+        @site = current_user.sites.find_by!(name: params[:name])
+        @available_color_palettes = ColorPalette.all
+        @available_font_pairs = FontPair.all
+        respond_to do |format|
+            @site.update(site_params)
+            updated_to_do = turbo_stream_params[:klass].constantize.new(@site)
+            format.turbo_stream { render turbo_stream: turbo_stream.replace(updated_to_do, partial: updated_to_do) }
+        end
     end
 
     # POST member.thatsite.io/sites
     def create
         @site = Site.new(site_params)
-        
         respond_to do |format|
             if @site.save
                 current_user.sites << @site
-                format.any(:html, :turbo_stream) { redirect_to edit_site_url(@site.name)}
+                format.any(:html, :turbo_stream) { redirect_to edit_site_url(@site)}
             else
                 updated_to_do = turbo_stream_params[:klass].constantize.new(@site)
                 format.turbo_stream { render turbo_stream: turbo_stream.replace(updated_to_do, partial: updated_to_do) }
@@ -44,7 +52,7 @@ class SitesController < ApplicationController
     private
 
     def site_params
-        params[:site].permit(:name)
+        params[:site].permit(:name, :color_palette_id, :font_pair_id)
     end
 
     def turbo_stream_params
