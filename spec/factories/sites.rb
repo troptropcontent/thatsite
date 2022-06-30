4494629b5d31a7c55033f4d37203fe4e410bc513
Tom Ecrepont <tomecrepont@gmail.com>
2022-06-30 14:01:35 +0200

    update site factory

--- spec/factories/sites.rb
+++ spec/factories/sites.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :site do
-    name{"test"}
+    name { 'test' }
     color_palette { ColorPalette.last || FactoryBot.create(:color_palette) }
     font_pair { FontPair.last || FactoryBot.create(:font_pair) }
-    business { Business.last || FactoryBot.create(:business)}
   end
 end
