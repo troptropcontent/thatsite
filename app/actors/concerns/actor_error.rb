45664e610ae83b1913bfd09a3e328c6917ee1221
Tom Ecrepont <tomecrepont@gmail.com>
2022-07-02 08:20:29 +0200

    create actor to create site and actor error

--- app/actors/concerns/actor_error.rb
+++ app/actors/concerns/actor_error.rb
@@ -1,9 +1,11 @@
-require "active_support/concern"
+# frozen_string_literal: true
+
+require 'active_support/concern'
 
 module ActorError
   extend ActiveSupport::Concern
 
-  def actor_error(type, message)
-    {actor: self.class, type: type, message: message}
+  def fail_with_validation_errors!(record)
+    fail! actor: self.class, type: :validation, errors: record.errors
   end
 end
