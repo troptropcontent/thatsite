8751787402d8166e9506095837dc41c1b4dd59ff
Tom Ecrepont <tomecrepont@gmail.com>
2022-07-19 16:45:23 +0200

    modularize color_palette

---
+++ app/models/core/style/color_palette.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: color_palettes
+#
+#  id         :bigint           not null, primary key
+#  first      :string
+#  second     :string
+#  third      :string
+#  fourth     :string
+#  fifth      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+module Core::Style
+    class ColorPalette < ApplicationRecord
+    end
+end
