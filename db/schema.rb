bed1bf8f33eaceee1c84c3148389a1ffe8daeea2
Tom Ecrepont <tomecrepont@gmail.com>
2022-07-19 17:37:27 +0200

    modularize office (#93)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_19_145034) do
+ActiveRecord::Schema.define(version: 2022_07_19_153245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,11 +104,12 @@ ActiveRecord::Schema.define(version: 2022_07_19_145034) do
     t.string "address_full", null: false
     t.string "address_complement"
     t.string "address_zipcode", null: false
-    t.string "addresse_city", null: false
+    t.string "address_city", null: false
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "phone"
+    t.string "address_country", null: false
     t.index ["business_id"], name: "index_offices_on_business_id"
   end
 
--- spec/factories/activity/offices.rb
+++ spec/factories/activity/offices.rb
@@ -5,10 +5,11 @@
 # Table name: offices
 #
 #  id                 :bigint           not null, primary key
+#  address_city       :string           not null
 #  address_complement :string
+#  address_country    :string           not null
 #  address_full       :string           not null
 #  address_zipcode    :string           not null
-#  addresse_city      :string           not null
 #  name               :string
 #  phone              :string
 #  created_at         :datetime         not null
@@ -23,13 +24,16 @@
 #
 #  fk_rails_...  (business_id => businesses.id)
 #
-FactoryBot.define do
-  factory :office do
-    business { Business.last || FactoryBot.create(:business) }
-    address_full { '38 rue ordener' }
-    address_complement { nil }
-    address_zipcode { '75018' }
-    addresse_city { 'Paris' }
-    name { nil }
+module Activity
+  FactoryBot.define do
+    factory :office, class: 'Activity::Office' do
+      business { Business.last || FactoryBot.create(:business) }
+      address_full { '38 rue ordener' }
+      address_complement { nil }
+      address_zipcode { '75018' }
+      address_city { 'Paris' }
+      address_country { 'FR' }
+      name { nil }
+    end
   end
 end
